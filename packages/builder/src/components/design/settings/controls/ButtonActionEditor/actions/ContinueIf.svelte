3c08ce9941cdb99fe8800e298a065ea7a1783487
IchthysMaranatha <94102122+IchthysMaranatha@users.noreply.github.com>
2023-03-29 14:00:52 +0530

    Add files via upload

--- packages/builder/src/components/design/settings/controls/ButtonActionEditor/actions/ContinueIf.svelte
+++ packages/builder/src/components/design/settings/controls/ButtonActionEditor/actions/ContinueIf.svelte
@@ -42,8 +42,8 @@
 
 <div class="root">
   <Body size="S">
-    Configure a condition to be evaluated which can stop further actions from
-    being executed.
+    Configure a condition to be evaluated which can stop further actions or jump
+    to another action.
   </Body>
   <Select
     bind:value={parameters.type}
